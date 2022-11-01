34eb2a96ebc44ac215197b90d52450189319d377
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-02 00:59:17 +0100

    Update MatchDataProcessor.java

--- src/main/java/com/valleon/ipldashboard/data/MatchDataProcessor.java
+++ src/main/java/com/valleon/ipldashboard/data/MatchDataProcessor.java
@@ -47,11 +47,12 @@ public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
     match.setTeam1(secondInningsTeam);
 
     match.setTossWinner(matchInput.getToss_winner());
-    match.setTossDecision(matchInput.getToss_decision());
-    match.setResult(matchInput.getResult());
-    match.setResultMargin(matchInput.getResult_margin());
-    match.setUmpire1 (matchInput.getUmpire1());
-    match.setUmpire2(matchInput.getUmpire2());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setMatchWinner(matchInput.getWinner());
+        match.setResult(matchInput.getResult());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
     
 
 
