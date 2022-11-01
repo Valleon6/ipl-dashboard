959ffa667c2fed7b43ac6e1b0ab8bb7bc0c220d4
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-02 00:59:13 +0100

    Update Team.java

--- src/main/java/com/valleon/ipldashboard/model/Team.java
+++ src/main/java/com/valleon/ipldashboard/model/Team.java
@@ -1,16 +1,26 @@
 package com.valleon.ipldashboard.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Team {
 
+    public Team(String string, long l) {
+    }
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private String teamName;
